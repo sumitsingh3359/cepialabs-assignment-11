444d9d5cb461cb8dc436fa2dc0cd883234cb037d
Sumit Kumar <sumit33@duck.com>
2025-12-01 09:23:02 +0530

    improved css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement, reset, incrementByAmount } from './app/counterSlice';
-import './App.css';
 
 function App() {
   // Selects the current count from the Redux store
